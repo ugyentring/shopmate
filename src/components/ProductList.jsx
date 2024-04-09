533907c94da598e517a364c445850a66a9e9bf82
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-04-09 21:53:12 +0600

    updated UI using useState and useEffect hook

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,17 +2,27 @@ import { useState, useEffect } from "react";
 
 export const ProductList = () => {
   const [products, setProducts] = useState([]);
-  console.log(products)
 
   useEffect(() => {
-    fetch("http://localhost:8000/posts")
+    fetch("http://localhost:8000/products")
       .then((response) => response.json())
       .then((data) => setProducts(data));
   }, []);
 
   return (
-    <div>
-      <h2>Hello</h2>
-    </div>
+    <section>
+      {products.map((product) => (
+        <div className="card" key={product.id}>
+          <p className="id">{product.id}</p>
+          <p className="name">{product.name}</p>
+          <p className="info">
+            <span>${product.price}</span>
+            <span className={product.in_stock ? "instock" : "unavailable"}>
+              {product.in_stock ? "In stock" : "Out of stock"}
+            </span>
+          </p>
+        </div>
+      ))}
+    </section>
   );
 };
